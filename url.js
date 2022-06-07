8acce10be8c507a764e36d6d21064ace77d2e839
Daphne-Zheng <106900314+Daphne-Zheng@users.noreply.github.com>
2022-06-07 12:50:02 +0800

    Update url.js

--- url.js
+++ url.js
@@ -13,7 +13,7 @@
 *風衣咖啡
 *牙膏印表機
 *印表機牙膏
-*啤酒起泡水
+*del 啤酒起泡水  'https://docs.google.com/forms/d/e/1FAIpQLSd-vOpJcdXbxesrZhZDwfWpEo6S4WDZ5KkcRP4GCZsePw45Gw/viewform',
 *起泡水啤酒
 *
 */	
@@ -24,6 +24,5 @@ const urls = [
 	'https://docs.google.com/forms/d/e/1FAIpQLSeiHNin6-z97XyHWFIIAWx1Pq2UTaev7g4F8gRblik9d1jOIA/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLScW6fgzVcljhEeuuAZPFrlMzVy0TN-X4yAmMgCDCjD8KGQU4A/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLScY4w-ZmeYmYL1PEvZ4A8jmkdaZYooHeWV5zsUoiahYWnUDuA/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSd-vOpJcdXbxesrZhZDwfWpEo6S4WDZ5KkcRP4GCZsePw45Gw/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLScyEgYY1A8HSGcdLVCa7OyY5GQzhtkFPif_dbh3kEAQWPGgdA/viewform'	
 ];
