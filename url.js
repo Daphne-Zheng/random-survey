559ba659cccd91308822515bbf9ae37edfc9af2d
Daphne-Zheng <106900314+Daphne-Zheng@users.noreply.github.com>
2022-06-09 23:05:02 +0800

    Update url.js

--- url.js
+++ url.js
@@ -20,5 +20,10 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLScY4w-ZmeYmYL1PEvZ4A8jmkdaZYooHeWV5zsUoiahYWnUDuA/viewform'
+	'https://docs.google.com/forms/d/e/1FAIpQLScbEF7TlNtJXfcmDykKChbIV7fBmSHf9S-WsbatsJs_aq4TEA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeiHNin6-z97XyHWFIIAWx1Pq2UTaev7g4F8gRblik9d1jOIA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLScW6fgzVcljhEeuuAZPFrlMzVy0TN-X4yAmMgCDCjD8KGQU4A/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLScY4w-ZmeYmYL1PEvZ4A8jmkdaZYooHeWV5zsUoiahYWnUDuA/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSd-vOpJcdXbxesrZhZDwfWpEo6S4WDZ5KkcRP4GCZsePw45Gw/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLScyEgYY1A8HSGcdLVCa7OyY5GQzhtkFPif_dbh3kEAQWPGgdA/viewform'
 ];
