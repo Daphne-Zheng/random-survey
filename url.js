d11df7368c23a9a32cf7cf3dba5798dcdb6546c9
Daphne-Zheng <106900314+Daphne-Zheng@users.noreply.github.com>
2022-06-09 20:28:06 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,7 +11,7 @@
 *網站順序
 *del 咖啡風衣
 *風衣咖啡
-*牙膏印表機
+*del 牙膏印表機
 *印表機牙膏
 *del 啤酒起泡水   
 *起泡水啤酒
@@ -21,7 +21,6 @@
 
 const urls = [
 	'https://docs.google.com/forms/d/e/1FAIpQLSeiHNin6-z97XyHWFIIAWx1Pq2UTaev7g4F8gRblik9d1jOIA/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScW6fgzVcljhEeuuAZPFrlMzVy0TN-X4yAmMgCDCjD8KGQU4A/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLScY4w-ZmeYmYL1PEvZ4A8jmkdaZYooHeWV5zsUoiahYWnUDuA/viewform',
 	'https://docs.google.com/forms/d/e/1FAIpQLScyEgYY1A8HSGcdLVCa7OyY5GQzhtkFPif_dbh3kEAQWPGgdA/viewform'	
 ];
