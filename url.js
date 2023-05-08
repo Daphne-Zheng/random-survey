41c218dd023306180c4d1b3cf89ccb2bbefaf141
Daphne-Zheng <106900314+Daphne-Zheng@users.noreply.github.com>
2023-05-08 20:02:07 +0800

    Update url.js

--- url.js
+++ url.js
@@ -15,13 +15,12 @@
 *低描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSdwevKIx0X8KuOnp81_maB7Raba_j2Wc8vlAxg-oRYoN4IRKw/viewform
 * DEL 低描述遠延伸：https://docs.google.com/forms/d/e/1FAIpQLSeyRYbtK2MhfoWm3kTkU6oY5j93HfLZHVTQDo88F4nLCQ0d9Q/viewform
 *高描述近延伸：https://docs.google.com/forms/d/e/1FAIpQLSejb9pV6NsysLHy3XCvf8jLIyTXkXmxDSCG2urpmB4irGbLNw/viewform
-*高描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSfiHUckPCl7uOtRFA6AXzQcYhvGY3zspHQCvhzDf22kChlHcg/viewform
+* DEL 高描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSfiHUckPCl7uOtRFA6AXzQcYhvGY3zspHQCvhzDf22kChlHcg/viewform
 * DEL 高描述遠延伸：https://docs.google.com/forms/d/e/1FAIpQLSeVj0g24BpOKrx0ts6_Yz6pgSv_5l-kvCGWvbBPFKLtaQqy6w/viewform
 */	
 
 
 const urls = [
 	'https://docs.google.com/forms/d/e/1FAIpQLSdwevKIx0X8KuOnp81_maB7Raba_j2Wc8vlAxg-oRYoN4IRKw/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSejb9pV6NsysLHy3XCvf8jLIyTXkXmxDSCG2urpmB4irGbLNw/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSfiHUckPCl7uOtRFA6AXzQcYhvGY3zspHQCvhzDf22kChlHcg/viewform'	
+	'https://docs.google.com/forms/d/e/1FAIpQLSejb9pV6NsysLHy3XCvf8jLIyTXkXmxDSCG2urpmB4irGbLNw/viewform'	
 ];
