814ccc59c513101d253f8d7a996f9e06f0c64de0
Daphne-Zheng <106900314+Daphne-Zheng@users.noreply.github.com>
2025-01-06 22:41:13 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,18 +9,9 @@
  */
 
 
-/*
-*網站順序
-* DEL 低描述近延伸：https://docs.google.com/forms/d/e/1FAIpQLSeN_AvFTUXJ_lqf6fXCvfmpBf4SsqGcdjFvGImVY6seH3uFaw/viewform
-* DEL 低描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSdwevKIx0X8KuOnp81_maB7Raba_j2Wc8vlAxg-oRYoN4IRKw/viewform
-* DEL 低描述遠延伸：https://docs.google.com/forms/d/e/1FAIpQLSeyRYbtK2MhfoWm3kTkU6oY5j93HfLZHVTQDo88F4nLCQ0d9Q/viewform
-*高描述近延伸：https://docs.google.com/forms/d/e/1FAIpQLSejb9pV6NsysLHy3XCvf8jLIyTXkXmxDSCG2urpmB4irGbLNw/viewform
-* DEL 高描述中延伸：https://docs.google.com/forms/d/e/1FAIpQLSfiHUckPCl7uOtRFA6AXzQcYhvGY3zspHQCvhzDf22kChlHcg/viewform
-* DEL 高描述遠延伸：https://docs.google.com/forms/d/e/1FAIpQLSeVj0g24BpOKrx0ts6_Yz6pgSv_5l-kvCGWvbBPFKLtaQqy6w/viewform
-*/	
-
 
 const urls = [
-	'https://www.baidu.com/'
-	'https://www.bilibili.com/'	
+	'https://www.wjx.cn/vm/Yo3PBMg.aspx#',
+	'https://www.wjx.cn/vm/Yo3PBMg.aspx#',	
+	'https://www.wjx.cn/vm/eJnolh4.aspx#'
 ];
